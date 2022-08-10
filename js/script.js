6bcbc3df7f79d7b6cbee473168b90e2040073a52
SergLiubimov <105861221+SergLiubimov@users.noreply.github.com>
2022-08-10 15:29:31 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        fade:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        
+    });
+    
+});
